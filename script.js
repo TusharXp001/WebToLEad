a97d1f9601fb3887d2ad056eaefc63b0543ca308
TusharXp001 <tusharip69@gmail.com>
2024-01-09 21:05:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function beforeLoad(){
+    let inputdate = document.querySelector('.inputdate');
+    let outputdate = document.querySelector('.outputdate');
+    console.log(inputdate);
+    console.log(outputdate);
+}
